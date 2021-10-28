6f71f3c4b2cb83786a3c4f8d58f24833401c10ba
Randy Fay <randy@randyfay.com>
2021-10-28 16:19:11 -0600

    More interactive

--- try-host-docker-internal.sh
+++ try-host-docker-internal.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "Starting nc listener on host side, listening on 9999"
 nc -l 0.0.0.0 9999 &
 ncpid=$!
 
-container=$(docker run -d --add-host host.docker.internal:host-gateway drud/ddev-webserver:v1.18.0 bash -c 'echo "Hello from inside the container" | nc host.docker.internal 9999')
+echo "Running nc talker in container - talking to 9999"
+docker run --add-host host.docker.internal:host-gateway drud/ddev-webserver:v1.18.0 bash -c 'echo "Hello from inside the container" | nc host.docker.internal 9999'
 
-kill $ncpid
+echo "Sleeping: We should have seen 'hello from inside'"
+sleep 5 && kill ${ncpid}
